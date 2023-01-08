f58e9432603262b62ea7feb314665a78b7b47507
Beatriz Aquino <beatrizzoldyck07@gmail.com>
2023-01-08 12:23:46 -0400

    desario tabuleiro

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+
+import styles from '../styles/Home.module.css'
+import Tabuleiro from '../components/Tabuleiro'
+ 
+
+export default function Home() {
+  return (
+    <div className={styles.linha}>
+      <Tabuleiro />
+
+    </div>
+  )
+}
